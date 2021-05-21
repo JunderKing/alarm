912c2cd705e8b1ab6dd4e69917a3ecacb47a11ed
JunderKing <junderking@163.com>
2021-05-21 15:17:32 +0800

    update

--- app/Http/Controllers/AlarmController.php
+++ app/Http/Controllers/AlarmController.php
@@ -9,6 +9,7 @@ class AlarmController extends Controller
 {
     public function sendAlarm() {
         $title = $this->param('title', 'nullable|string', '通知');
+        $this->param('passwd', 'required|in:a6c649665ef9b8bc68f486926f4789e3');
         $content = $this->check('content', 'required|string');
         Models\Alarm::send($title, $content);
 
